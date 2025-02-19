9df5bff0ea1a1693b48aaed97b5d32d7e3ca81e4
VI-2 <125826839+VI-2@users.noreply.github.com>
2025-02-19 19:22:37 +0100

    Added Images and two levels

---
+++ new_dino_adventure/assets/tiles/House (112 x 96).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="House (112 x 96)" tilewidth="16" tileheight="16" tilecount="90" columns="15">
+ <image source="../Documents/University/Studies/Operational Systems/FlutterGame/dino_adventures/assets/images/Seasonal Tilesets/5 - Misc. universal tiles/House (112 x 96).png" width="240" height="96"/>
+</tileset>
---
+++ new_dino_adventure/assets/tiles/grassland.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="grassland" tilewidth="16" tileheight="16" tilecount="170" columns="17">
+ <image source="Seasonal Tilesets/1 - Grassland/Terrain (16 x 16).png" width="272" height="160"/>
+ <tile id="21">
+  <objectgroup draworder="index" id="8">
+   <object id="10" x="0.125" y="0.375" width="16" height="15.25"/>
+  </objectgroup>
+ </tile>
+</tileset>
