3f91f01b2db3113096baaea535c5d95d0fa3c4c9
VI-2 <125826839+VI-2@users.noreply.github.com>
2025-02-19 21:03:00 +0100

    added more assets

---
+++ new_dino_adventure/assets/tiles/Bubbling_Oil_frames (16 x 32).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Bubbling_Oil_frames (16 x 32)" tilewidth="16" tileheight="16" tilecount="8" columns="4">
+ <image source="../images/5 - Misc. universal tiles/Bubbling_Oil_frames (16 x 32).png" width="64" height="32"/>
+</tileset>
